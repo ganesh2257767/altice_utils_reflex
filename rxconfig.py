5a11ab449c00054bec5806cc8b20443fcf5a3142
Ganesh <ganeshkutty@alticeusa.com>
2024-10-28 17:27:25 +0000

    Env changes

--- rxconfig.py
+++ rxconfig.py
@@ -1,6 +1,11 @@
 import reflex as rx
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+db_url = os.getenv('DB_URL')
 config = rx.Config(
     app_name="Altice_Utils",
-    db_url="postgresql+psycopg2://postgres:root@localhost/postgres"
+    db_url=db_url
 )
