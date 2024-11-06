d658269300980b058c665d7a48a6c2c753b4fb4c
Ganesh <ganeshkutty@alticeusa.com>
2024-11-06 12:26:45 +0000

    Some config changes

--- rxconfig.py
+++ rxconfig.py
@@ -1,5 +1,6 @@
-import reflex as rx
 import os
+
+import reflex as rx
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,5 +8,5 @@ load_dotenv()
 db_url = os.getenv('DB_URL')
 config = rx.Config(
     app_name="Altice_Utils",
-    db_url=db_url
+    db_url=db_url,
 )
